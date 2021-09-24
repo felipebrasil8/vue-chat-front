bedea4d70adff8c103a8de8d05837a7b834ede3b
Felipe Brasil <felipe.almeida@locaweb.com.br>
2021-09-24 10:45:14 -0300

    Add components Navbar and Footer

---
+++ src/components/Foot/Foot.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer>
+    <hr/>
+    <p class="foot">Chat-Vue</p>
+  </footer>
+</template>
+<script>
+export default {
+  name: "Foot"
+};
+</script>
+
+<style lang="css">
+.foot {
+  color: #5db7de !important;
+}
+.foot a {
+    color: #5db7de;
+  }
+.foot a:hover {
+    color: #a39b8b !important;
+  }
+</style>
---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="navbar navbar-dark chat-app">
+    <router-link to="/" class="nav-link" >Rooms</router-link>
+    <span class="navbar-text">
+      <router-link to="/logout" class="btn btn-logout btn-sm">Logout</router-link>
+    </span>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+}
+</script>
+
+<style scoped>
+.chat-app {
+  background: #5db7de !important;
+}
+.nav-link {
+  color: #ffffff;
+}
+.btn-logout {
+  background-color: #5db7de;
+  border-color: none;
+  color: #fff;
+}
+.btn-logout:hover {
+  background-color: darken(#07020d, 20%);
+  border-color: darken(#07020d, 20%);
+  color: #fff;
+}
+</style>
